b12773c397f7005cb837d4d6b292f03792d003d4
iamdramaticleo <iamdramaticleo@gmail.com>
2025-08-20 18:31:46 +0300

    Refactor mouse_press to button_press in input callbacks
    Standardize terminology by renaming `mouse_press` to `button_press` across input callbacks and related methods. This improves consistency in naming conventions and reflects the broader scope of button inputs.

--- core/base/window_input_callbacks.hpp
+++ core/base/window_input_callbacks.hpp
@@ -6,7 +6,7 @@ namespace core::base
 {
     struct window_input_callbacks
     {
-        std::function<void(input::codes    key, bool state)>   key_press { };
-        std::function<void(input::codes button, bool state)> mouse_press { };
+        std::function<void(input::codes    key, bool state)>    key_press { };
+        std::function<void(input::codes button, bool state)> button_press { };
     };
 }
