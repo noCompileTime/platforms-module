5bf0bb6c08b59d6cdaae3a693bb886c926507539
iamdramaticleo <iamdramaticleo@gmail.com>
2025-08-21 16:39:46 +0300

    Adjust alignment of `update` method declarations for consistency

--- core/base/window_events.hpp
+++ core/base/window_events.hpp
@@ -10,7 +10,7 @@ namespace core::base
     public:
         virtual auto     init(const std::unique_ptr<Window>& window) -> void = 0;
         virtual auto  release(const std::unique_ptr<Window>& window) -> void = 0;
-        virtual auto   update()                                const -> void = 0;
+        virtual auto   update()                               const  -> void = 0;
 
         virtual ~WindowEvents() = default;
 
--- windows/window_events.hpp
+++ windows/window_events.hpp
@@ -9,7 +9,7 @@ namespace windows
     public:
         auto    init(const std::unique_ptr<core::base::Window>& window) -> void override;
         auto release(const std::unique_ptr<core::base::Window>& window) -> void override;
-        auto  update()                                            const -> void override;
+        auto  update()                                           const  -> void override;
 
         static auto process_message(HWND hwnd, uint32_t msg, uintptr_t wparam, intptr_t lparam) -> intptr_t;
 
