f5aa5351caa9e6dbfa38d61a3e81a1d4ed036b5e
iamdramaticleo <iamdramaticleo@gmail.com>
2025-08-27 00:35:39 +0300

    Refactor: Reorganize window input and event callbacks/definitions
    Moved window input and event-related structs to a dedicated 'window' namespace for better clarity and organization. Updated includes and references throughout the module to align with the new structure. Added TODO for separating build directories in CMakePresets.json.

--- core/base/window_events_callbacks.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace core::base
-{
-    struct window_events_callbacks
-    {
-        std::function<void()> close { };
-    };
-}
--- core/base/window_input_codes.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace core::base
-{
-    using window_input_codes = std::unordered_map<int32_t, input::code>;
-}
