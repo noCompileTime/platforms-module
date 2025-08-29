68a00aa5f0c1d681c46a22a078e486cfb7b7a0c9
iamdramaticleo <iamdramaticleo@gmail.com>
2025-08-29 20:54:05 +0300

    Fix: Align code style and field ordering in InputManager
    Adjusted member field order and standardized spacing for improved readability and consistency in `input_manager.hpp`.

--- core/input_manager.hpp
+++ core/input_manager.hpp
@@ -7,16 +7,16 @@ namespace core
     class InputManager
     {
     public:
-        auto update()                                     -> void;
-        auto update(input::code code, input::state state) -> void;
+        auto     update()                                     -> void;
+        auto     update(input::code code, input::state state) -> void;
+        auto is_pressed(input::code code) const -> bool;
 
-        auto    is_pressed(input::code code) const -> bool;
-        auto input_actions()                       -> InputActions&;
+        auto input_actions() -> InputActions&;
 
     private:
-                 InputActions _input_actions;
-
         input::states_changes _states_changes;
         input::states         _states;
+
+        InputActions _input_actions;
     };
 }
