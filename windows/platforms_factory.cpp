1b3a382077bec3a0a3243647eaaff00bf6dcc565
iamdramaticleo <iamdramaticleo@gmail.com>
2025-08-30 16:54:52 +0300

    Refactor: Add `create_window_context` to `PlatformsFactory`
    Introduced `create_window_context` method in `PlatformsFactory` for `WindowContext` creation. Updated CMakeLists and adjusted includes to integrate `WindowContext` support.

--- windows/platforms_factory.cpp
+++ windows/platforms_factory.cpp
@@ -1,6 +1,7 @@
 #include "platforms_factory.hpp"
 
 #include "window.hpp"
+#include "window_context.hpp"
 #include "window_events.hpp"
 #include "window_input.hpp"
 
@@ -9,6 +10,11 @@ namespace windows
     auto PlatformsFactory::create_window() -> std::unique_ptr<core::base::Window>
     {
         return std::make_unique<Window>();
+    }
+
+    auto PlatformsFactory::create_window_context() -> std::unique_ptr<core::base::WindowContext>
+    {
+        return std::make_unique<WindowContext>();
     }
 
     auto PlatformsFactory::create_window_events() -> std::unique_ptr<core::base::WindowEvents>
