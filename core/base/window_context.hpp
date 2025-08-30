e68ccbbf9d07be6363203d758d4506d0b001e66f
iamdramaticleo <iamdramaticleo@gmail.com>
2025-08-30 16:54:52 +0300

    Refactor: Add `create_window_context` to `PlatformsFactory`
    Introduced `create_window_context` method in `PlatformsFactory` for `WindowContext` creation. Updated CMakeLists and adjusted includes to integrate `WindowContext` support.

--- core/base/window_context.hpp
+++ core/base/window_context.hpp
@@ -7,10 +7,10 @@ namespace core::base
     class WindowContext
     {
     public:
-        virtual auto  create(const std::unique_ptr<Window>& window) -> void = 0;
+        virtual auto    create(const std::unique_ptr<Window>& window) -> void = 0;
 
-        virtual auto destroy()  -> void = 0;
-        virtual auto  update()  -> void = 0;
+        virtual auto   destroy()   const -> void = 0;
+        virtual auto    update()   const -> void = 0;
 
         virtual ~WindowContext() = default;
     };
