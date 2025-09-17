73d4b056260ed9e7aad35b7f02f61839abecfa5b
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-17 15:39:29 +0300

    Add Windows-specific constants for window context and pixel configuration
    Introduced `window_context.hpp` and `window_context_pixel.hpp` with defined constants for Windows platform configurations. These constants streamline referencing configuration values in Windows-related modules.

---
+++ windows/constants/window_context.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace windows::constants
+{
+    constexpr auto major_version = 0x2091;
+    constexpr auto minor_version = 0x2092;
+    constexpr auto       profile = 0x9126;
+    constexpr auto         flags = 0x2094;
+
+    constexpr auto  core_profile = 0x00000001;
+    constexpr auto    no_error   = 0x00000008;
+}
