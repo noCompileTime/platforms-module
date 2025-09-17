191443843fe5f341f2e66d07689ff8c9bf3559af
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-17 15:57:23 +0300

    Add PFNWGLSWAPINTERVALPROC and refactor main variable declarations
    Introduced PFNWGLSWAPINTERVALPROC and corresponding inline variable in window_context.hpp to expand functionality. Refactored main.cpp by adding `const` qualifiers and improving alignment for better readability and immutability where applicable.

--- windows/functions/window_context.hpp
+++ windows/functions/window_context.hpp
@@ -2,9 +2,11 @@
 
 namespace windows::functions
 {
-    using PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC shared_context,       const int32_t* attrib_list) -> HGLRC;
+    using PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC shared_context, const int32_t* attrib_list) -> HGLRC;
     using PFNWGLCHOOSEPIXELFORMATPROC    = auto(WINAPI*)(HDC hdc, const int32_t* attrib_list, const float* attrib_weights, uint32_t max_formats , int32_t* format_list, uint32_t* num_formats) -> int32_t;
+    using PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t interval) -> int32_t;
 
     inline PFNWGLCREATECONTEXTATTRIBSPROC create_context_attribs;
     inline PFNWGLCHOOSEPIXELFORMATPROC    choose_pixel_format;
+    inline PFNWGLSWAPINTERVALPROC         swap_interval;
 }
