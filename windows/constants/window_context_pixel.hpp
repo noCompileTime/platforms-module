d95dc14273df70f5ea8ea80199f11781fdb27a9a
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-17 16:04:35 +0300

    Refactor: Improve alignment and formatting in Windows `WindowContext` headers for better consistency

--- windows/constants/window_context_pixel.hpp
+++ windows/constants/window_context_pixel.hpp
@@ -5,17 +5,17 @@ namespace windows::constants
     constexpr auto      acceleration = 0x2003;
     constexpr auto full_acceleration = 0x2027;
 
-    constexpr auto samples        = 0x2042;
-    constexpr auto samples_buffer = 0x2041;
-    constexpr auto  double_buffer = 0x2011;
-    constexpr auto    srgb_buffer = 0x20a9;
+    constexpr auto    samples        = 0x2042;
+    constexpr auto    samples_buffer = 0x2041;
+    constexpr auto     double_buffer = 0x2011;
+    constexpr auto       srgb_buffer = 0x20a9;
 
-    constexpr auto draw_to_window = 0x2001;
-    constexpr auto support_opengl = 0x2010;
+    constexpr auto    draw_to_window = 0x2001;
+    constexpr auto    support_opengl = 0x2010;
 
-    constexpr auto     color_type = 0x2013;
-    constexpr auto           rgba = 0x202b;
-    constexpr auto     color_bits = 0x2014;
-    constexpr auto     depth_bits = 0x2022;
-    constexpr auto   stencil_bits = 0x2023;
+    constexpr auto        color_type = 0x2013;
+    constexpr auto              rgba = 0x202b;
+    constexpr auto        color_bits = 0x2014;
+    constexpr auto        depth_bits = 0x2022;
+    constexpr auto      stencil_bits = 0x2023;
 }
--- windows/functions/window_context.hpp
+++ windows/functions/window_context.hpp
@@ -2,9 +2,9 @@
 
 namespace windows::functions
 {
-    using PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC shared_context, const int32_t* attrib_list) -> HGLRC;
-    using PFNWGLCHOOSEPIXELFORMATPROC    = auto(WINAPI*)(HDC hdc, const int32_t* attrib_list, const float* attrib_weights, uint32_t max_formats , int32_t* format_list, uint32_t* num_formats) -> int32_t;
-    using PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t interval) -> int32_t;
+     using PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC, const int32_t*)                                              -> HGLRC;
+     using PFNWGLCHOOSEPIXELFORMATPROC    = auto(WINAPI*)(HDC hdc,        const int32_t*, const float*, uint32_t, int32_t*, uint32_t*) -> int32_t;
+     using PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t)                                                                     -> int32_t;
 
     inline PFNWGLCREATECONTEXTATTRIBSPROC create_context_attribs;
     inline PFNWGLCHOOSEPIXELFORMATPROC    choose_pixel_format;
