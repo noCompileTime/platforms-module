c9cc2223ba5d26e643ce656f525506b9a0c15bcd
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-17 23:10:25 +0300

    Refactor: Merge `window_context_pixel.hpp` into `window_context.hpp` to simplify Windows context constants management

--- windows/constants/window_context.hpp
+++ windows/constants/window_context.hpp
@@ -9,4 +9,21 @@ namespace windows::constants
 
     constexpr auto  core_profile = 0x00000001;
     constexpr auto    no_error   = 0x00000008;
+
+    constexpr auto      acceleration = 0x2003;
+    constexpr auto full_acceleration = 0x2027;
+
+    constexpr auto    samples        = 0x2042;
+    constexpr auto    samples_buffer = 0x2041;
+    constexpr auto     double_buffer = 0x2011;
+    constexpr auto       srgb_buffer = 0x20a9;
+
+    constexpr auto    draw_to_window = 0x2001;
+    constexpr auto    support_opengl = 0x2010;
+
+    constexpr auto        color_type = 0x2013;
+    constexpr auto              rgba = 0x202b;
+    constexpr auto        color_bits = 0x2014;
+    constexpr auto        depth_bits = 0x2022;
+    constexpr auto      stencil_bits = 0x2023;
 }
--- windows/headers.hpp
+++ windows/headers.hpp
@@ -5,6 +5,5 @@
 #include <windows.h>
 
 #include "constants/window_context.hpp"
-#include "constants/window_context_pixel.hpp"
 
 #include "functions/window_context.hpp"
