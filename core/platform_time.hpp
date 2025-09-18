0a70e57d8e3cd89ed38069e8c19553a259f9b0db
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-18 17:00:53 +0300

    Refactor: Adjust formatting in `PlatformTime` for improved alignment consistency

--- core/platform_time.hpp
+++ core/platform_time.hpp
@@ -18,8 +18,8 @@ namespace core
     private:
         static constexpr auto max_delta_time { 1.0 / 30.0f };
 
-        inline static float _delta_time { };
-        inline static float _total_time { };
+        inline static float  _delta_time { };
+        inline static float  _total_time { };
 
         time_point   _start_time { };
         time_point _current_time { };
