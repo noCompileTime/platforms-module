b01284e1e57b4de1427142f4934fdc288363be30
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-21 21:22:28 +0300

    Add PlatformFunctions class to centralize platform initialization
    Introduced a new `PlatformFunctions` class to handle platform-specific initialization logic, streamlining the main entry point. Updated `main.cpp` to utilize this new class and modified the `CMakeLists.txt` to include the new source file. This improves code modularity and readability.

---
+++ core/platform_functions.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace core
+{
+    class PlatformFunctions
+    {
+    public:
+        static auto init(const std::unique_ptr<base::PlatformFactory>& factory) -> void;
+    };
+}
