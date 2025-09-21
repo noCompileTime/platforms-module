d2a15cd87aa40f2f2c757b5536c8de002aff2864
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-21 15:19:40 +0300

    Refactor: Improve alignment and formatting in `WindowManager` and `InputManager` headers for consistency

--- core/input_manager.hpp
+++ core/input_manager.hpp
@@ -11,12 +11,12 @@ namespace core
         auto  update(input::code code, input::state state) -> void;
         auto pressed(input::code code) const -> bool;
 
-        auto input_actions() -> InputActions&;
+        auto input_actions()  -> InputActions&;
 
     private:
         input::states_changes _states_changes;
         input::states_values  _states_values;
 
-        InputActions _input_actions;
+        InputActions    _input_actions;
     };
 }
--- core/window_manager.hpp
+++ core/window_manager.hpp
@@ -7,13 +7,13 @@ namespace core
     public:
         auto    init(const std::unique_ptr<base::PlatformFactory>& factory, const window::configuration& configuration) -> void;
 
-        auto release()         const -> void;
-        auto  update()         const -> void;
+        auto release()          const -> void;
+        auto  update()          const -> void;
 
-        auto  window()         const -> base::Window&;
-        auto  window_context() const -> base::WindowContext&;
-        auto  window_events () const -> base::WindowEvents&;
-        auto  window_input  () const -> base::WindowInput&;
+        auto  window()          const -> base::Window&;
+        auto  window_context()  const -> base::WindowContext&;
+        auto  window_events ()  const -> base::WindowEvents&;
+        auto  window_input  ()  const -> base::WindowInput&;
 
     private:
         std::unique_ptr<base::Window>        _window;
