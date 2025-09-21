b3340ca686717eb3817767bd8e757a190bf8e1b5
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-21 15:15:14 +0300

    Refactor: Reorder destructors for consistency and add `title` method to `Window`
    Reorganized destructors in `PlatformFactory`, `WindowInput`, and `WindowEvents` for better consistency. Introduced `title` method in `Window` for setting window titles. Updated related interfaces and implementations.

--- core/base/window_events.hpp
+++ core/base/window_events.hpp
@@ -7,11 +7,11 @@ namespace core::base
     class WindowEvents
     {
     public:
-        virtual ~WindowEvents() = default;
-
         virtual auto     init(const window_ptr& window)       -> void = 0;
         virtual auto  release(const window_ptr& window) const -> void = 0;
         virtual auto   update()                         const -> void = 0;
+
+        virtual ~WindowEvents()  = default;
 
         window::events_callbacks callbacks;
     };
