77542d54c1acf91adad2f08734b92ce36d085ddb
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-26 00:01:08 +0300

    Refactor: Adjust method alignment in platform factory headers for consistency

--- core/base/platform_factory.hpp
+++ core/base/platform_factory.hpp
@@ -12,10 +12,10 @@ namespace core::base
     class PlatformFactory
     {
     public:
-        virtual auto create_window()             const -> std::unique_ptr<Window>            = 0;
-        virtual auto create_window_context()     const -> std::unique_ptr<WindowContext>     = 0;
-        virtual auto create_window_events ()     const -> std::unique_ptr<WindowEvents>      = 0;
-        virtual auto create_window_input  ()     const -> std::unique_ptr<WindowInput>       = 0;
+        virtual auto create_window            () const -> std::unique_ptr<Window>            = 0;
+        virtual auto create_window_context    () const -> std::unique_ptr<WindowContext>     = 0;
+        virtual auto create_window_events     () const -> std::unique_ptr<WindowEvents>      = 0;
+        virtual auto create_window_input      () const -> std::unique_ptr<WindowInput>       = 0;
 
         virtual auto create_platform_functions() const -> std::unique_ptr<PlatformFunctions> = 0;
         virtual auto create_platform_monitor  () const -> std::unique_ptr<PlatformMonitor>   = 0;
