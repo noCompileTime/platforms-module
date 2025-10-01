a26b56f903f00efbe273b09886313237ff048910
iamdramaticleo <iamdramaticleo@gmail.com>
2025-10-01 11:47:34 +0300

    Add buffer deletion and storage handling to OpenGL functions
    Introduced support for `glDeleteBuffers` and `glNamedBufferStorage` in OpenGL functions to manage buffer deletion and storage. Additionally, updated type definitions for consistency and added a new `depth_buffer` constant for better rendering context support. Minor formatting adjustments were also made for improved readability.

--- windows/functions/window_context.hpp
+++ windows/functions/window_context.hpp
@@ -2,9 +2,9 @@
 
 namespace windows::functions
 {
-     using PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC, const int32_t*)                                              -> HGLRC;
-     using PFNWGLCHOOSEPIXELFORMATPROC    = auto(WINAPI*)(HDC hdc,        const int32_t*, const float*, uint32_t, int32_t*, uint32_t*) -> int32_t;
-     using PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t)                                                                     -> int32_t;
+    using  PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC, const int32_t*)                                              -> HGLRC;
+    using  PFNWGLCHOOSEPIXELFORMATPROC    = auto(WINAPI*)(HDC hdc,        const int32_t*, const float*, uint32_t, int32_t*, uint32_t*) -> int32_t;
+    using  PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t)                                                                     -> int32_t;
 
     inline PFNWGLCREATECONTEXTATTRIBSPROC create_context_attribs;
     inline PFNWGLCHOOSEPIXELFORMATPROC    choose_pixel_format;
