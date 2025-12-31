fe91c5819c8bf9b8940ced185dd25077bd714a1d
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-31 19:42:56 +0200

    Add noexcept specifier to functions across modules
    This commit ensures that functions which are guaranteed not to throw exceptions are explicitly marked with the noexcept specifier. This improves code clarity, helps with optimizations, and ensures better interoperability with exception-unsafe code. Adjustments span multiple modules, including graphics, platforms, and runtime.

--- core/input_actions.cpp
+++ core/input_actions.cpp
@@ -2,12 +2,12 @@
 
 namespace core
 {
-    auto InputActions::set_action(const input::code code, const input::action& action) -> void
+    auto InputActions::set_action(const input::code code, const input::action& action) noexcept -> void
     {
         _actions[code] = action;
     }
 
-    auto InputActions::execute(const input::code code) -> void
+    auto InputActions::execute(const input::code code) noexcept -> void
     {
         if (const auto iterator  = _actions.find(code);
                        iterator != _actions.end())
