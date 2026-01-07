14e267076f544304d44cdc616d67ce74e1d486d2
iamdramaticleo <iamdramaticleo@gmail.com>
2026-01-07 21:03:31 +0200

    "Fix formatting and add missing inline variable"
    Corrected formatting for `PFNWGLSWAPINTERVALPROC` to improve readability and added the missing `wglSwapInterval` inline variable initialization. These changes ensure consistency and prevent potential usage issues.

--- windows/functions/window_context.hpp
+++ windows/functions/window_context.hpp
@@ -4,9 +4,11 @@ namespace windows::functions
 {
     using  PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC, const int32_t*)                                              -> HGLRC;
     using  PFNWGLCHOOSEPIXELFORMATPROC    = auto(WINAPI*)(HDC hdc,        const int32_t*, const float*, uint32_t, int32_t*, uint32_t*) -> int32_t;
-    using  PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t)                                                                     -> int32_t;
+
+    using  PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t) -> int32_t;
 
     inline PFNWGLCREATECONTEXTATTRIBSPROC wglCreateContextAttribs;
     inline PFNWGLCHOOSEPIXELFORMATPROC    wglChoosePixelFormat;
+
     inline PFNWGLSWAPINTERVALPROC         wglSwapInterval;
 }
