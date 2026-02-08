cf302c206dd8c75fd9a5f142cd6e89cf12d0f782
iamdramaticleo <iamdramaticleo@gmail.com>
2026-02-09 00:19:53 +0200

    Add TODO to refactor `status` enum in `window/status.hpp`

--- core/window/status.hpp
+++ core/window/status.hpp
@@ -2,7 +2,7 @@
 
 namespace core::window
 {
-    enum status
+    enum status // TODO move this into some platform_status? and be called from a callback or something?
     {
         pixel_format_not_available,
         pixel_format_not_supported,
