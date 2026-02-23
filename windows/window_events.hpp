847af120b80f0a27334a4718f649328f0e5b4caa
iamdramaticleo <iamdramaticleo@gmail.com>
2026-02-23 13:52:56 +0200

    Refactor static constant definitions for consistency.
    Updated formatting and placement of `constexpr static` keywords for consistency across files. Renamed `max_delta_time` to `delta_time_max` for clarity and standardization in the time module.

--- windows/window_events.hpp
+++ windows/window_events.hpp
@@ -15,7 +15,7 @@ namespace windows
         static auto process_btn_message(HWND hwnd, uint32_t code, core::input::state state) -> void;
         static auto process_key_message(HWND hwnd, uint32_t code, core::input::state state) -> void;
 
-        static constexpr auto events_prop_key = "events";
-        static constexpr auto  input_prop_key = "input";
+        constexpr   static auto events_prop_key = "events";
+        constexpr   static auto  input_prop_key = "input";
     };
 }
