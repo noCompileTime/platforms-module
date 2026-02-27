678c529f720abcc83c60c3aa838c9609d60a2795
iamdramaticleo <iamdramaticleo@gmail.com>
2026-02-27 21:25:10 +0200

    Reorder methods in `InputState` for better readability and adjust spacing in `update` method.

--- core/input_state.cpp
+++ core/input_state.cpp
@@ -2,19 +2,9 @@
 
 namespace core
 {
-    auto InputState::active(const input::code code) const noexcept -> input::state
-    {
-        return _active.at(code);
-    }
-
-    auto InputState::before(const input::code code) const noexcept -> input::state
-    {
-        return _before.at(code);
-    }
-
     auto InputState::update(const input::code code, const input::state active) noexcept -> void
     {
-        const auto before = std::exchange(_active[code],  active);
+        const auto before = std::exchange(_active[code] , active);
                                           _before[code] = before;
 
         if (before == input::state::released && active == input::state::pressed)
@@ -24,5 +14,15 @@ namespace core
                 _changes.emplace_back(code);
             }
         }
+    }
+
+    auto InputState::active(const input::code code) const noexcept -> input::state
+    {
+        return _active.at(code);
+    }
+
+    auto InputState::before(const input::code code) const noexcept -> input::state
+    {
+        return _before.at(code);
     }
 }
