fac91284db1e4f7d7962493db92a48997344e790
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-09 00:15:42 +0200

    Refactor OpenGL function definitions for clarity
    Moved `glCreateVertexArrays` and `glDeleteVertexArrays` to inline declarations near their type definitions for better organization. Added a TODO comment in the Windows context file to clarify parameter names, improving maintainability.

--- windows/functions/window_context.hpp
+++ windows/functions/window_context.hpp
@@ -2,7 +2,7 @@
 
 namespace windows::functions
 {
-    using  PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC, const int32_t*)                                              -> HGLRC;
+    using  PFNWGLCREATECONTEXTATTRIBSPROC = auto(WINAPI*)(HDC hdc, HGLRC, const int32_t*)                                              -> HGLRC; // TODO add the parameters name
     using  PFNWGLCHOOSEPIXELFORMATPROC    = auto(WINAPI*)(HDC hdc,        const int32_t*, const float*, uint32_t, int32_t*, uint32_t*) -> int32_t;
 
     using  PFNWGLSWAPINTERVALPROC         = auto(WINAPI*)(int32_t) -> int32_t;
