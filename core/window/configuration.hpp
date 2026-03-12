6685aba5433dc76ef1ea3d821cce0b2ebea8d2ed
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-12 10:48:01 +0200

    Add TODO to clarify context separation in configuration settings

--- core/window/configuration.hpp
+++ core/window/configuration.hpp
@@ -8,7 +8,7 @@ namespace core::window
 
         int32_t    width   { };
         int32_t    height  { };
-        int32_t    samples { };
+        int32_t    samples { }; // TODO separate what is for the context in another config?
 
         bool    fullscreen { };
         bool     maximized { };
