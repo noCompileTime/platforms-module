f8a5bec8ab9bdcd191c487d7183af9c07b6aaf3c
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-13 11:43:44 +0200

    Reorder `update()` method in `InputManager` for improved code structure and readability.

--- core/input_manager.cpp
+++ core/input_manager.cpp
@@ -2,16 +2,6 @@
 
 namespace core
 {
-    auto InputManager::update() noexcept -> void
-    {
-        for (const auto& change : _state)
-        {
-            _actions.execute(change);
-        }
-
-        _state._changes.clear(); // TODO maybe mark them as executed? to not make clear again and again?
-    }
-
     auto InputManager::actions() noexcept -> InputActions&
     {
         return _actions;
@@ -20,5 +10,15 @@ namespace core
     auto InputManager::state() noexcept -> InputState&
     {
         return _state;
+    }
+
+    auto InputManager::update() noexcept -> void
+    {
+        for (const auto& change : _state)
+        {
+            _actions.execute(change);
+        }
+
+        _state._changes.clear(); // TODO maybe mark them as executed? to not make clear again and again?
     }
 }
