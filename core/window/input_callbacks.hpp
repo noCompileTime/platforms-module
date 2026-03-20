f3c15aebf59ff7cb806e09e718f10046c52579e0
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-20 12:35:42 +0200

    Fix inconsistent spacing in `on_mouse_motion` and `on_mouse_scroll` function declarations

--- core/window/input_callbacks.hpp
+++ core/window/input_callbacks.hpp
@@ -7,7 +7,7 @@ namespace core::window
         std::function<void(input::code btn, input::state state)> on_btn_press { };
         std::function<void(input::code key, input::state state)> on_key_press { };
 
-        std::function<void(int32_t x, int32_t y)>  on_mouse_motion { };
-        std::function<void(int32_t    delta)>      on_mouse_scroll { };
+        std::function<void(int32_t x, int32_t y)> on_mouse_motion { };
+        std::function<void(int32_t delta)>        on_mouse_scroll { };
     };
 }
