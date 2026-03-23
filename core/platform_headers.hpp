fced22bcfee6f5ba3cc2582e29731714a0cc4b00
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-23 11:21:55 +0200

    Add TODO comment in `platform_headers.hpp` about replacing `std::string` with `std::string_view`.

--- core/platform_headers.hpp
+++ core/platform_headers.hpp
@@ -3,7 +3,7 @@
 #include <any>
 #include <array>
 #include <memory>
-#include <string>
+#include <string> // TODO to see if we can replace it with std::string_view
 
 #include <algorithm>
 #include <functional>
