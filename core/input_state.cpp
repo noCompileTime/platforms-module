6ac3cbe1eb35bb56d86c2f58e03e9346707cb4ad
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-26 10:09:05 +0200

    Fix inconsistent bracket placement in `input_state.cpp` and add TODOs to consolidate button and key message processing in `window_events.hpp`.

--- core/input_state.cpp
+++ core/input_state.cpp
@@ -5,8 +5,8 @@ namespace core
     auto InputState::update(const input::code code, const input::state state) noexcept -> void
     {
         if (const auto before =  std::exchange(_states[code], state);
-                       before == input::state::released && state == input::state::pressed)
-        {
+                       before == input::state::released && state == input::state::pressed) {
+
             if (!std::ranges::contains(_changes, code))
             {
                 _changes.emplace_back(code);
--- windows/window_events.hpp
+++ windows/window_events.hpp
@@ -12,8 +12,8 @@ namespace windows
         static auto process_any_message(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)  -> LRESULT;
 
     private:
-        static auto process_btn_message(HWND hwnd, uint32_t code, core::input::state state) -> void;
-        static auto process_key_message(HWND hwnd, uint32_t code, core::input::state state) -> void;
+        static auto process_btn_message(HWND hwnd, uint32_t code, core::input::state state) -> void; // TODO make just one function
+        static auto process_key_message(HWND hwnd, uint32_t code, core::input::state state) -> void; // TODO make just one function
     };
 
         static constexpr auto events_prop_key = "events_";
