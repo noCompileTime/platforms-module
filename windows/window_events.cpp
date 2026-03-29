f1f9a3742b58e56e1451727e94df61a688538738
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-29 16:02:34 +0300

    Fix spacing inconsistencies in `window_events.cpp` for `width` and `height` variable initialization

--- windows/window_events.cpp
+++ windows/window_events.cpp
@@ -127,8 +127,8 @@ namespace windows
                 if (const auto window_events = static_cast<WindowEvents*>(GetProp(hwnd, events_prop_key));
                                window_events->callbacks.on_resize)
                 {
-                    const auto width  =  LOWORD(lparam);
-                    const auto height =  HIWORD(lparam);
+                    const auto width  = LOWORD(lparam);
+                    const auto height = HIWORD(lparam);
 
                     window_events->callbacks.on_resize(width, height);
                 }
