3c27c9914e1c2040b1de0ef704f6a7d1e914c60b
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-02 15:42:15 +0300

    Update Windows platform headers: replace `WINAPI` with `APIENTRY` and standardize integer types
    Simplify and align function pointer definitions by replacing `WINAPI` with `APIENTRY`. Standardize integer types to `std::uint32_t` for consistent usage in `window.hpp`.

--- windows/functions/window_context.hpp
+++ windows/functions/window_context.hpp
@@ -2,12 +2,12 @@
 
 namespace windows::functions
 {
-     using PFNWGLCREATECONTEXTATTRIBSPROC = HGLRC(WINAPI*)(HDC hdc, HGLRC share_context, const int32_t* attribs);
+     using PFNWGLCREATECONTEXTATTRIBSPROC = HGLRC(APIENTRY*)(HDC hdc, HGLRC share_context, const int32_t* attribs);
     inline PFNWGLCREATECONTEXTATTRIBSPROC wglCreateContextAttribs;
 
-     using PFNWGLCHOOSEPIXELFORMATPROC = int32_t(WINAPI*)(HDC hdc, const int32_t* int_attribs, const float* float_attribs, uint32_t max_formats, int32_t* formats, uint32_t* num_formats);
+     using PFNWGLCHOOSEPIXELFORMATPROC = int32_t(APIENTRY*)(HDC hdc, const int32_t* int_attribs, const float* float_attribs, uint32_t max_formats, int32_t* formats, uint32_t* num_formats);
     inline PFNWGLCHOOSEPIXELFORMATPROC wglChoosePixelFormat;
 
-     using PFNWGLSWAPINTERVALPROC = int32_t(WINAPI*)(int32_t interval);
+     using PFNWGLSWAPINTERVALPROC = int32_t(APIENTRY*)(int32_t interval);
     inline PFNWGLSWAPINTERVALPROC wglSwapInterval;
 }
