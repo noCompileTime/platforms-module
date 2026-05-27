4d728bcc76f34e7400ec1d4c1937e43bb5690bab
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-27 11:51:24 +0300

    Refactor includes and library links for cleanup and consistency
    Removed unnecessary includes and macros in headers, cleaned up redundant TODO comments, and adjusted library link order in CMake files for better clarity and organization. These changes improve maintainability and align with planned module transitions.

--- windows/platform_internal.hpp
+++ windows/platform_internal.hpp
@@ -1,4 +1,6 @@
 #pragma once // TODO remove this file at some point - when moving to vulkan
 
+#include <windows.h>
+
 #include "constants/window_context.hpp"
 #include "functions/window_context.hpp"
