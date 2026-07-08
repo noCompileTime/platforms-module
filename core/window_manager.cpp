6ee367ff7690afc324af71f8ec405365566db3f4
iamdramaticleo <iamdramaticleo@gmail.com>
2026-07-08 19:44:35 +0300

    Reorder `WindowManager::window` implementation to improve method grouping

--- core/window_manager.cpp
+++ core/window_manager.cpp
@@ -35,6 +35,11 @@ namespace core
         _events->update();
     }
 
+    auto WindowManager::window() const noexcept -> base::Window&
+    {
+        return *_window;
+    }
+
     auto WindowManager::context() const noexcept -> base::WindowContext&
     {
         return *_context;
@@ -48,10 +53,5 @@ namespace core
     auto WindowManager::input() const noexcept -> base::WindowInput&
     {
         return *_input;
-    }
-
-    auto WindowManager::window() const noexcept -> base::Window&
-    {
-        return *_window;
     }
 }
